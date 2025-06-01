7192dc60f53d3f43c6bb907950a1b6e3ac421811
Maria Isabel <mimarinm@gmail.com>
2025-06-01 14:42:43 +0200

    Add dashboard layout

--- ai-melt-ui/src/app/app.ts
+++ ai-melt-ui/src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './layout/header/header';
+import { Footer } from './layout/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [Header, Footer, RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss'],
 })
+
 export class App {
   protected title = 'ai-melt-ui';
 }
--- ai-melt-ui/src/main.ts
+++ ai-melt-ui/src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { importProvidersFrom }                  from '@angular/core';
 import { BrowserAnimationsModule }              from '@angular/platform-browser/animations';
-import { App }                                  from './app/app';
+import { AppComponent }                                  from './app/app.component';
 import { AppRoutingModule }                    from './app/app-routing.module';
 
 // Angular Material modules — each from its own package
@@ -12,7 +12,7 @@ import { MatListModule }     from '@angular/material/list';
 import { MatIconModule }     from '@angular/material/icon';
 import { MatButtonModule }   from '@angular/material/button';
 
-bootstrapApplication(App, {
+bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(
       BrowserModule,
